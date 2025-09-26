1be3921f1a37888d234d7838412192f30337bd0c
yerald231ger <yerald231ger@gmail.com>
2025-09-25 22:30:31 -0600

    Refactor app layout to include user form and appointment calendar components with updated styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppointmentCalendarComponent } from './appointment-calendar/appointment-calendar.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppointmentCalendarComponent],
+  imports: [RouterOutlet, AppointmentCalendarComponent, UserFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
