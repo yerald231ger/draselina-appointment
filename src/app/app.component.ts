df2e1e29b17250f7c09dee813c649857775d69a7
yerald231ger <yerald231ger@gmail.com>
2025-09-25 22:24:43 -0600

    Add appointment calendar component and update global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppointmentCalendarComponent } from './appointment-calendar/appointment-calendar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppointmentCalendarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
